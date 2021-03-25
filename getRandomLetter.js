0b0c7ac5b01230b3b73007e6ffe4104292b47e70
mara2t <martirosyan9955@gmail.com>
2021-03-25 19:07:57 +0400

    add getRandomLetter Function

---
+++ getRandomLetter.js
@@ -0,0 +1,5 @@
+function getRandomLetter(){
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
+  const randomNumber = Math.floor(Math.random()*alphabet.length) 
+  return alphabet[randomNumber] 
+ }
