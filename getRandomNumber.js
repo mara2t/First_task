fff9edb71b047fd42dca39ce525e1fa82caafccb
mara2t <martirosyan9955@gmail.com>
2021-03-25 19:11:37 +0400

    return content

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,9 +1,7 @@
-function getRandomLetter(){
-  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
-  const randomNumber = Math.floor(Math.random()*alphabet.length) 
-  return alphabet[randomNumber] 
- }
-getRundomLetter()
-getRundomLetter()
-getRundomLetter()
-getRundomLetter()
+function getRundomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+getRundomNumber();
+getRundomNumber();
+getRundomNumber();
+getRundomNumber();
